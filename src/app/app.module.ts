c9d38ae08102e9f244d6e75f4c07700f27349d75
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-06-12 13:52:02 -0300

    add: crypto,nfts and daos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CryptosComponentComponent } from './cryptos-component/cryptos-component.component';
+import { NftsComponentComponent } from './nfts-component/nfts-component.component';
+import { DaosComponentComponent } from './daos-component/daos-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CryptosComponentComponent,
+    NftsComponentComponent,
+    DaosComponentComponent
   ],
   imports: [
     BrowserModule
