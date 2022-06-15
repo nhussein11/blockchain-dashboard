389cf720d796e57121e822d001ea794d26e62fda
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-06-15 12:27:50 -0300

    modified: cryptoService

--- src/app/services/cryptos.service.ts
+++ src/app/services/cryptos.service.ts
@@ -5,36 +5,33 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CryptosService {
 
-  constructor(private _httpClient: HttpClient) { }
+export class CryptosService {
+  
+  HEADERS = {
+    //My own key; 
+    'X-CMC_PRO_API_KEY': 'b61a8fc1-81dc-4ab2-8fa5-475d5ddf4e08',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Origin':'*'
+  }
+
+  constructor(private http: HttpClient) { }
 
   getCryptos():Observable<any>{
     let url = '/v1/cryptocurrency/listings/latest';
-    // let url ='https://jsonplaceholder.typicode.com/todos';
-    return this._httpClient.get<any>(url,
-        {
-          headers: {
-                  //My own key; 
-                  'X-CMC_PRO_API_KEY': 'b61a8fc1-81dc-4ab2-8fa5-475d5ddf4e08',
-                  'Accept': 'application/json',
-                  'Access-Control-Allow-Origin':'*'
-                }
-        }
+    
+    return this.http.get<any>(url,
+        { headers: this.HEADERS }
       );
+
+
+
   }
   getCryptoDetails(id:string):Observable<any>{
     let url = '/v1/cryptocurrency/info?id='+id.toString();
-    // let url ='https://jsonplaceholder.typicode.com/todos';
-    return this._httpClient.get<any>(url,
-        {
-          headers: {
-                  //My own key; 
-                  'X-CMC_PRO_API_KEY': 'b61a8fc1-81dc-4ab2-8fa5-475d5ddf4e08',
-                  'Accept': 'application/json',
-                  'Access-Control-Allow-Origin':'*'
-                }
-        }
+    
+    return this.http.get<any>(url,
+        { headers: this.HEADERS }
       );
   }
   
