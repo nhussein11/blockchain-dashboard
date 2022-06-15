13e29cabdd08205401dd3390283406bc44fcdeff
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-06-15 11:13:17 -0300

    clean: app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,38 @@
+import { AppComponent } from './app.component';
+import { CryptosComponentComponent } from './pages/cryptocurrencies/cryptos-component/cryptos-component.component';
+import { CryptoDetailsComponent } from './pages/cryptocurrencies/crypto-details/crypto-details.component';
+import { DaosComponentComponent } from './pages/daos/daos-component/daos-component.component';
+import { DaoDetailsComponent } from './pages/daos/dao-details/dao-details.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { NftsComponentComponent } from './pages/nfts/nfts-component/nfts-component.component';
+import { NftDetailsComponent } from './pages/nfts/nft-details/nft-details.component';
+import { NotFoundPageComponentComponent } from './pages/not-found-page-component/not-found-page-component.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-
-import { CryptosComponentComponent } from './pages/cryptocurrencies/cryptos-component/cryptos-component.component';
-import { NftsComponentComponent } from './pages/nfts/nfts-component/nfts-component.component';
-import { DaosComponentComponent } from './pages/daos/daos-component/daos-component.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { NotFoundPageComponentComponent } from './pages/not-found-page-component/not-found-page-component.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CryptoDetailsComponent } from './pages/cryptocurrencies/crypto-details/crypto-details.component';
-// import { AlertModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { DaoDetailsComponent } from './pages/daos/dao-details/dao-details.component';
-import { NftDetailsComponent } from './pages/nfts/nft-details/nft-details.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
+
     CryptosComponentComponent,
     NftsComponentComponent,
     DaosComponentComponent,
     HomeComponentComponent,
-    NotFoundPageComponentComponent,
 
     CryptoDetailsComponent,
     DaoDetailsComponent,
     NftDetailsComponent,
+
+    NotFoundPageComponentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    // AlertModule.forRoot(),
     InfiniteScrollModule,
   ],
   providers: [],
