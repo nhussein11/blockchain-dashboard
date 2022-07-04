5ae3832c18a4bbd54bebd38536cfca8c039aa112
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-04 17:01:58 -0300

    add: ng-bootstrap

--- src/app/pages/nfts/nft-details/nft-details.component.ts
+++ src/app/pages/nfts/nft-details/nft-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Nft } from 'src/app/models/Nfts';
-
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-nft-details',
   templateUrl: './nft-details.component.html',
@@ -12,13 +12,16 @@ export class NftDetailsComponent implements OnInit {
   nft: Nft = {} as Nft;
   data: string = '';
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute,
+            private modal: NgbModal
+    ) { }
 
   ngOnInit(): void {
-    
+
     this.data = this._route.snapshot.paramMap.get('data')!;
     this.nft = JSON.parse(this.data);
-
+    // console.log(this.nft.name)
+    
   }
 
 }
