705a765958a09b5d64ef2f6f414ca79c469e16d8
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-08 11:30:17 -0300

    add: preloadingStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 
 const routes: Routes = [
@@ -8,15 +8,21 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   },
-
- 
+  {
+    path:'',
+    redirectTo:'',
+    pathMatch: 'full'
+  }
 
 ]
 
 @NgModule({
 
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes,{
+      enableTracing:true,
+      preloadingStrategy:PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
