798adccd84cdb6f52a8d0e2929a75e7bf4ef8a4a
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-08 11:45:46 -0300

    delete: unnecesary import on NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,4 @@
 import { AppComponent } from './app.component';
-import { CryptosComponentComponent } from './pages/cryptocurrencies/cryptos-component/cryptos-component.component';
-import { CryptoDetailsComponent } from './pages/cryptocurrencies/crypto-details/crypto-details.component';
-import { DaosComponentComponent } from './pages/daos/daos-component/daos-component.component';
-import { DaoDetailsComponent } from './pages/daos/dao-details/dao-details.component';
-import { HomeComponentComponent } from './pages/home-component/home-component.component';
-import { NftsComponentComponent } from './pages/nfts/nfts-component/nfts-component.component';
-import { NftDetailsComponent } from './pages/nfts/nft-details/nft-details.component';
-import { NotFoundPageComponentComponent } from './pages/not-found-page-component/not-found-page-component.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -18,7 +10,6 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,   
-    HomeComponentComponent  
   ],
   imports: [
     BrowserModule,
