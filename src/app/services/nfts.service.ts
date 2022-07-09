630375faa60295c68964855c3060163ff4873ba1
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-09 10:45:02 -0300

    add: exchanges styles

--- src/app/services/nfts.service.ts
+++ src/app/services/nfts.service.ts
@@ -14,12 +14,14 @@ export class NftsService {
 
   getNfts(owner: string): Observable<Nft[]> {
     let url = `https://eth-mainnet.alchemyapi.io/nft/v2/demo/getNFTs/?owner=${owner}`;
-  
+
     return this.http.get<any>(url)
       .pipe(
         map((res) => {
           return (res.ownedNfts).map(
+
             (ownedNfts: OwnedNft) => {
+
               let nft: Nft = ownedNfts.metadata
 
               if (nft.image?.split('/').includes("v")) {
@@ -27,14 +29,11 @@ export class NftsService {
                 nft.image = undefined
               }
 
-              nft.image?.startsWith('ipfs://')
-                ? nft.image = 'https://ipfs.io/ipfs' + nft.image.substring(6)
-                : nft.image
+              if (nft.image?.startsWith('ipfs://')) {
+                nft.image = 'https://ipfs.io/ipfs' + nft.image.substring(6)
+              }
 
-              
-                if (!(nft && (Object.keys(nft).length === 0))) {
-                // console.log(nft)
-                
+              if (!(nft && (Object.keys(nft).length === 0))) {
                 return nft
               } else return;
             }
