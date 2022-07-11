f650ef33292bed6f729fef11332aa299581ea309
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-11 15:33:41 -0300

    add: crypto https headers on service

--- src/app/services/cryptos.service.ts
+++ src/app/services/cryptos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Cryptocurrency, CryptocurrencyDetailsGraphic } from '../models/Cryptocurrency';
@@ -12,13 +12,10 @@ import { CryptocurrencyDetails } from '../models/Cryptocurrency-Details';
 
 export class CryptosService {
 
-  private HEADERS = {
-    //My own key; 
-    'X-CMC_PRO_API_KEY': 'b61a8fc1-81dc-4ab2-8fa5-475d5ddf4e08',
-    'Accept': 'application/json',
-    // 'Accept-Encoding': 'deflate, gzip',
-    'Access-Control-Allow-Origin': '*'
-  }
+  private headers = new HttpHeaders()
+          .set('X-CMC_PRO_API_KEY', 'b61a8fc1-81dc-4ab2-8fa5-475d5ddf4e08')
+          .set('Accept', 'application/json')
+          .set('Access-Control-Allow-Origin', '*')
 
   constructor(private http: HttpClient) { }
 
@@ -26,7 +23,7 @@ export class CryptosService {
     let url = '/v1/cryptocurrency/listings/latest';
 
     return this.http.get<any>(url,
-      { headers: this.HEADERS, observe: 'response' })
+      { headers : this.headers, observe: 'response' })
       .pipe(
         map((resp) => resp.body.data)
       );
@@ -36,7 +33,7 @@ export class CryptosService {
     let url = '/v1/cryptocurrency/info?id=' + id;
 
     return this.http.get<any>(url,
-      { headers: this.HEADERS, observe: 'response' })
+      { headers: this.headers, observe: 'response' })
       .pipe(
         map(({body:{data}}) => data[id])
       );
@@ -46,7 +43,7 @@ export class CryptosService {
     let url = '/v1/cryptocurrency/quotes/latest?id=' + id;
     let cryptoPercetages: number[] = [];
     return this.http.get<any>(url,
-      { headers: this.HEADERS, observe: 'response' })
+      { headers: this.headers, observe: 'response' })
       .pipe(
         map(({body:{data}}) => {
           let { 
