3cf949803fd08fe93d1137dc4e246d015bbbfcca
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-12 12:03:32 -0300

    add: nfts details safeUrl

--- src/app/modules/nfts/nft-details/nft-details.component.ts
+++ src/app/modules/nfts/nft-details/nft-details.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { ActivatedRoute, Params } from '@angular/router';
 import { Nft } from 'src/app/models/Nfts';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-nft-details',
   templateUrl: './nft-details.component.html',
@@ -16,7 +17,8 @@ export class NftDetailsComponent implements OnInit,OnChanges {
   
 
   constructor(private _route: ActivatedRoute,
-              private activeModal: NgbActiveModal
+              private activeModal: NgbActiveModal,
+              private _sanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
@@ -29,6 +31,13 @@ export class NftDetailsComponent implements OnInit,OnChanges {
 
   closeModal() {
     this.activeModal.close("Modal closed!");
+  }
+
+  getSantizeUrl(url : string | undefined){
+    if(url){
+      return this._sanitizer.bypassSecurityTrustUrl(url);
+    }else{return;}
+    
   }
 
 
