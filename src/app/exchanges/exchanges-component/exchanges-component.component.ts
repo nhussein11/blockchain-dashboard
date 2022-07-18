03b10e6b8c64a27974e67449e9444d8354c1bbb2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-18 12:20:43 -0300

    add: scroll top button

--- src/app/exchanges/exchanges-component/exchanges-component.component.ts
+++ src/app/exchanges/exchanges-component/exchanges-component.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject, Injectable, OnInit } from '@angular/core';
+import { Component, Host, HostListener, Inject, OnInit } from '@angular/core';
 import { Exchange } from 'src/app/models/Exchange';
 import { ExchangesService } from 'src/app/services/exchanges.service';
 
@@ -13,25 +13,28 @@ export class ExchangesComponentComponent implements OnInit {
   exchanges: Exchange[] = [];
 
   perPage: number = 8;
-  page:number=1;
+  page: number = 1;
+
   scrollHeight: number = 500;
+  showButton: boolean = false;
 
   searchText: string = '';
 
   exchangesLoaded: boolean = false;
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private _exchangesService: ExchangesService) { }
 
 
   ngOnInit(): void {
-    this.loadExchanges(this.perPage,this.page);
+    this.loadExchanges(this.perPage, this.page);
   }
 
-  loadExchanges(perPage: number, page:number) {
-    this._exchangesService.getExchanges(perPage,page).subscribe(
+  loadExchanges(perPage: number, page: number) {
+    this._exchangesService.getExchanges(perPage, page).subscribe(
       (response: Exchange[]) => {
-        this.exchanges=this.exchanges.concat(response);    
+        this.exchanges = this.exchanges.concat(response);
         this.exchangesLoaded = true;
       }, ((err) => {
         console.log('Error')
@@ -40,11 +43,20 @@ export class ExchangesComponentComponent implements OnInit {
     );
   }
 
-
-  scollDown() {
+  scollDown(): void {
     this.page += 1;
-    this.loadExchanges(this.perPage,this.page);
+    this.loadExchanges(this.perPage, this.page);
   }
 
+  scrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
 
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+
+    this.showButton = (yOffSet || scrollTop) > this.scrollHeight;
+  }
 }
