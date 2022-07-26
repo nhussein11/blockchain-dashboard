bc9c77d01aa24f87e13a768db390aea6aed85548
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-26 11:26:24 -0300

    add: auth module routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    
     LoginComponent,
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
