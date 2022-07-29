a5992f834edc15389e3cabc469fa050f8da8da4f
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-28 23:14:07 -0300

    add: testing first initial route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 require('./database')
 
+app.use(require('./routes/index'))
 app.listen(3000);
 
 console.log('server listening on: ', 3000);
