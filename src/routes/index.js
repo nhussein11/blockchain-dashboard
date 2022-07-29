e6c587c74ef65e87284669a6f4ea109aa33d0f55
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-29 10:24:59 -0300

    add: signin user

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
-const {Router} = require('express');
+const {Router, json} = require('express');
 
 const router = Router();
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 router.get('/', (req,res)=>{
     console.log("hola")
@@ -10,12 +11,33 @@ router.get('/', (req,res)=>{
 
 
 router.post('/signup', async (req,res)=>{
-    console.log(req.body)
+    
     const {name, email, username, password} = req.body;
     const newUser = new User({name, email, username, password})
     await newUser.save()
-    console.log(newUser)
-    res.send('Saved user')
+
+    const token = jwt.sign({_id: newUser._id}, 'secretKey');
+
+    res.status(200).json({token})
 });
+
+router.post('/signin', async (req,res)=>{
+    
+    const {username, password} = req.body;
+    const user = await User.findOne({username})
+    
+    if(!user){
+        res.status(401).send("Username doesn't exist!")
+    }
+
+    if(user.password !== password){
+        res.status(401).send("Password invalid!")
+    }
+
+    const token = jwt.sign({_id: user._id}, 'secretKey');
+
+    res.status(200).json({token})
+});
+
 
 module.exports = router;
