b36fae740ecab5a315a5d27f75c9cfc29304afb5
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-29 17:29:41 -0300

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('./database');
 
+app.use(cors());
 app.use(express.json());
-app.use('/api',require('./routes/index'));
+app.use('/api/auth',require('./routes/index'));
 
 app.listen(3000);
 
