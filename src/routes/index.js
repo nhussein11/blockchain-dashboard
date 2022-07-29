c7e4e800163906f923f1bd3faedea9bbfa9716b7
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-07-28 23:18:41 -0300

    add: nodemon -D

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const {Router} = require('express');
 
 const router = Router();
+const User = require('../models/User')
 
 router.get('/', (req,res)=>{
     res.send("hello world")
+});
+
+
+router.post('/singup', (req,res)=>{
+    res.send("registered")
 });
 
 module.exports = router;
