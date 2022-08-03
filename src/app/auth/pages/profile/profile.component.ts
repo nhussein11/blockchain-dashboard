e4d4e07e1fc0ae13b012b766566096130bc307d9
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-03 12:28:18 -0300

    add: profile form behaviour

--- src/app/auth/pages/profile/profile.component.ts
+++ src/app/auth/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from '../../models/User';
 import { AuthService } from '../../services/auth.service';
@@ -9,7 +9,7 @@ import { emailPattern } from '../../validation-patterns/validation-patterns';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
 
   
 
@@ -21,24 +21,29 @@ export class ProfileComponent implements OnInit {
   })
 
   isModifying:boolean=false;
+  initialValues:Object={};
+
 
 
 
   constructor(private _auth: AuthService, private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    
+    
+
     const id: string  = this._auth.getId()!;
     this.form.disable();
-    
     this._auth.profile(id).subscribe(
       ({ user } : { user: User }) => {
         
         const { name, email, username, password } = user;
 
-        this.form.controls['name'].setValue(name) ;
-        this.form.controls['email'].setValue(email)
-        this.form.controls['username'].setValue(username)
-        this.form.controls['password'].setValue(password)
+        this.form.controls['name'].setValue(name);
+        this.form.controls['email'].setValue(email);
+        this.form.controls['username'].setValue(username);
+        this.form.controls['password'].setValue(password);
+        this.initialValues = this.form.value;
       }
     )
   }
@@ -49,7 +54,20 @@ export class ProfileComponent implements OnInit {
   }
 
   modify(){
-    this.form.enable();
-    
+    if (this.isModifying){
+      this.form.disable();
+      this.isModifying=false;
+    }else{
+      this.form.enable();
+      this.isModifying=true;
+    }
   }
+
+  ngOnDestroy() {
+    if (this.initialValues != this.form.value) {
+      console.log(this.form.value)
+      
+        //update changes on db
+    }
+}
 }
