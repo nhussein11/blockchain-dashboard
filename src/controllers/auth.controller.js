5a049be31df436fa584706adde6fbe3e5d80a0bc
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-04 10:32:08 -0300

    add: auth.controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,82 @@
+const User = require('../models/User')
+const { ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+
+const getAllUsers = async (req,res)=>{
+    const users = await User.find()
+    res.status(200).send({"users":  users})
+}
+
+const signUp =  async (req,res)=>{
+    const {name, email, username, password} = req.body;
+    const newUser = new User({name, email, username, password})
+    await newUser.save()
+
+    const token = jwt.sign({_id: newUser._id}, 'secretKey');
+    res.status(200).json({token})
+}
+
+const signIn = async (req,res)=>{
+    const {username, password} = req.body;
+    const user = await User.findOne({username})
+    if(!user){
+        res.status(401).send("Username doesn't exist!")
+    }
+    if(user.password !== password){
+        res.status(401).send("Password invalid!")
+    }
+    const token = jwt.sign({_id: user._id}, 'secretKey');
+    res.status(200).json({token,id:user._id})
+}
+
+const forgotPassword =  async (req,res)=>{
+    
+    const {email} = req.body;
+    const user = await User.findOne({email})
+
+    if(!user){
+        res.status(401).send("Username doesn't exist!")
+    }
+    
+    const {password} = user;
+    res.status(200).json({'password':password})
+}
+
+const getProfile = async(req,res)=>{
+    const id =  req.userId;
+    const user = await User.findOne( {_id: ObjectId(id)})
+    res.status(200).json({user})
+}
+
+const updateProfile = async(req,res)=>{
+    const update = req.body;
+    const filter = {_id: (req.userId)}
+    const userToUpdate = await User.findOneAndUpdate( filter, update, { new: true })
+    res.status(200).json({userToUpdate})
+}
+
+
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        res.status(401).send("Unauthorized request")
+    }
+
+    const token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') { res.status(401).send("Unauthorized request") }
+
+    const payload = jwt.verify(token, 'secretKey')
+    req.userId = payload._id;
+
+    next();
+}
+
+module.exports = {
+    getAllUsers,
+    signUp,
+    signIn,
+    forgotPassword,
+    getProfile,
+    updateProfile,
+    verifyToken
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,91 +1,21 @@
-const {Router, json} = require('express');
+const { Router } = require('express');
+const { getAllUsers, 
+        signUp, 
+        signIn, 
+        forgotPassword, 
+        getProfile, 
+        updateProfile,
+        verifyToken } = require('../controllers/auth.controller')
 
 const router = Router();
-const User = require('../models/User')
-const jwt = require('jsonwebtoken')
 
-const { ObjectId } = require('mongodb');
-const mongoose = require("mongoose");
-
-router.get('/', async (req,res)=>{
-    const users = await User.find()
-    res.send("Users:" , users)
-});
-
-router.post('/signup', async (req,res)=>{
-    
-    const {name, email, username, password} = req.body;
-    const newUser = new User({name, email, username, password})
-    await newUser.save()
-
-    const token = jwt.sign({_id: newUser._id}, 'secretKey');
-
-    res.status(200).json({token})
-});
-
-router.post('/signin', async (req,res)=>{
-    
-    
-    const {username, password} = req.body;
-    
-    const user = await User.findOne({username})
-    
-    
-    if(!user){
-        res.status(401).send("Username doesn't exist!")
-    }
-    
-    if(user.password !== password){
-        res.status(401).send("Password invalid!")
-    }
-
-    const token = jwt.sign({_id: user._id}, 'secretKey');
-
-    res.status(200).json({token,id:user._id})
-});
-
-
-router.post('/forgot-password', async (req,res)=>{
-    
-    const {email} = req.body;
-    const user = await User.findOne({email})
-
-    if(!user){
-        res.status(401).send("Username doesn't exist!")
-    }
-    
-    const {password} = user;
-    res.status(200).json({'password':password})
-});
-
-
-
-
-router.get('/profile/:id',verifyToken, async(req,res)=>{
-    const id =  req.userId;
-    const user = await User.findOne( {_id: ObjectId(id)})
-    res.status(200).json({user})
-});
-
-router.patch('/profile/:id',verifyToken, async(req,res)=>{
-    const update = req.body;
-    const filter = {_id: (req.userId)}
-    const userToUpdate = await User.findOneAndUpdate( filter, update, { new: true })
-    res.status(200).json({userToUpdate})
-})
+router
+    .get('/', getAllUsers)
+    .post('/signup', signUp)
+    .post('/signin', signIn)
+    .post('/forgot-password', forgotPassword)
+    .get('/profile/:id', verifyToken, getProfile)
+    .patch('/profile/:id', verifyToken, updateProfile)
 
 module.exports = router;
 
-function verifyToken(req,res,next){
-    if(!req.headers.authorization){
-        res.status(401).send("Unauthorized request")
-    }
-
-    const token = req.headers.authorization.split(' ')[1]
-    if(token==='null') {res.status(401).send("Unauthorized request")}
-
-    const payload = jwt.verify(token,'secretKey')
-    req.userId = payload._id;
-
-    next();
-}
